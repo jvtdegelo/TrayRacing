38b8de1ad2bf1f435af61353812bf47175abda73
Gabriel Zambelli <gabrielzsb.comp@usp.br>
2024-05-19 11:47:48 -0300

    fix checkpoint position

--- Assets/Scripts/Countdown/CountdownController.cs
+++ Assets/Scripts/Countdown/CountdownController.cs
@@ -3,45 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CountdownController : MonoBehaviour {
+public class CountdownController : MonoBehaviour
+{
 
     public GameObject countDown;
+    private TMPro.TextMeshProUGUI countdownText;
     public AudioSource getReady;
     public AudioSource goAudio;
     public GameObject lapTimer;
-    public GameObject car;
-    private CarController carController;
+    public GameObject[] cars;
+    private CarController[] carControllers;
 
-    void Start() {
+    void Start()
+    {
+        // disables all car controllers
+        carControllers = new CarController[cars.Length];
+
+        for (int i = 0; i < cars.Length; i++)
+        {
+            carControllers[i] = cars[i].GetComponent<CarController>();
+            carControllers[i].enabled = false;
+        }
+
+        countdownText = countDown.GetComponent<TMPro.TextMeshProUGUI>();
+
         StartCoroutine(CountStart());
     }
 
-    IEnumerator CountStart() {
+    IEnumerator CountStart()
+    {
         yield return new WaitForSeconds(0.5f);
-        countDown.GetComponent<TMPro.TextMeshProUGUI>().text = "3";
+        countdownText.text = "3";
         getReady.Play();
         countDown.SetActive(true);
+
         yield return new WaitForSeconds(1);
         countDown.SetActive(false);
-        countDown.GetComponent<TMPro.TextMeshProUGUI>().text = "2";
+        countdownText.text = "2";
         getReady.Play();
         countDown.SetActive(true);
+
         yield return new WaitForSeconds(1);
         countDown.SetActive(false);
-        countDown.GetComponent<TMPro.TextMeshProUGUI>().text = "1";
+        countdownText.text = "1";
         getReady.Play();
         countDown.SetActive(true);
+
         yield return new WaitForSeconds(1);
         countDown.SetActive(false);
-        countDown.GetComponent<TMPro.TextMeshProUGUI>().text = "GO!";
-        countDown.GetComponent<TMPro.TextMeshProUGUI>().color = new Color32(143,214,138,255);
+        countdownText.text = "GO!";
+        countdownText.color = new Color32(143, 214, 138, 255);
 
         goAudio.Play();
         countDown.SetActive(true);
 
-        lapTimer.SetActive(true);        
-        carController = car.GetComponent<CarController>();
-        carController.enabled = true;
+        lapTimer.SetActive(true);
+
+        foreach (CarController carController in carControllers)
+        {
+            carController.enabled = true;
+        }
 
     }
 }
