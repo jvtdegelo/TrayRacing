e5cc484644c6bce627039161931fe947f5ab1c48
GabZamba <gabrielzsb.comp@usp.br>
2024-05-20 19:54:58 -0300

    fix minor errors and reorganize files

--- Assets/Scripts/Car/CarCreator.cs
+++ Assets/Scripts/Car/CarCreator.cs
@@ -15,18 +15,38 @@ public class CarCreator : MonoBehaviour
             selectedCarPrefab = carPrefabs[Random.Range(0, carPrefabs.Length)];
 
         // instanciates the selected car
+        InstantiateNormalCar();
+
+        // instanciates the giant minimap car 
+        InstantiateMinimapCar();
+    }
+
+    void Update() { }
+
+
+    // instanciates the selected car
+    private void InstantiateNormalCar()
+    {
         GameObject carInstance = Instantiate(selectedCarPrefab, transform);
         carInstance.transform.localPosition = new Vector3(0f, -1.0f, 0f);
         carInstance.name = "CarModel";
-        carController = this.GetComponent<CarController>();
 
-        if (carController != null)
+        if (TryGetComponent(out carController))
             carController.SetCarRigidbody(Instantiate(carRigidbodyPrefab, transform));
 
         AddParticleControllerToWheels(carInstance);
     }
 
-    void Update() { }
+    // instanciates the giant minimap car 
+    private void InstantiateMinimapCar()
+    {
+        GameObject carInstanceGiant = Instantiate(selectedCarPrefab, transform);
+
+        carInstanceGiant.transform.localPosition = new Vector3(0f, 50f, 0f);
+        carInstanceGiant.transform.localScale = new Vector3(18f, 18f, 18f);
+        carInstanceGiant.name = "CarModelGiantForMinimap";
+        RemoveShadows(carInstanceGiant);
+    }
 
     private void AddParticleControllerToWheels(GameObject car)
     {
@@ -58,6 +78,18 @@ public class CarCreator : MonoBehaviour
                 if (wheelName == "wheel_frontLeft") carController.SetFrontLeftWheel(wheel);
                 if (wheelName == "wheel_frontRight") carController.SetFrontRightWheel(wheel);
             }
+        }
+    }
+
+    private void RemoveShadows(GameObject car)
+    {
+        Renderer[] renderers = car.GetComponentsInChildren<Renderer>();
+
+        // Iterate over all Renderer components
+        foreach (Renderer renderer in renderers)
+        {
+            Debug.Log("Renderer found in: " + renderer.gameObject.name);
+            renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
         }
     }
 }
