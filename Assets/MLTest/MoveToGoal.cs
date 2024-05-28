e9e383d5f5dfaebb8b450d8f5fd13a70beff3960
Gabriel Zambelli <gabrielzsb.comp@usp.br>
2024-05-27 23:17:13 -0300

    add MLAgents test

---
+++ Assets/MLTest/MoveToGoal.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+
+        // base.CollectObservations(sensor);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 4f;
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-1.5f, 1.5f), 0, Random.Range(-1.5f, 1.5f));
+        targetTransform.localPosition = new Vector3(Random.Range(2, 6), 0, Random.Range(-2, 2));
+        // base.OnEpisodeBegin();
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log(other.gameObject.name);
+        if (other.gameObject.TryGetComponent<Goal>(out Goal goal))
+        {
+            Debug.Log("Won");
+            SetReward(1f);
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        if (other.gameObject.TryGetComponent<Wall>(out Wall wall))
+        {
+            Debug.Log("Lost");
+            SetReward(-1f);
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
+    }
+
+}
