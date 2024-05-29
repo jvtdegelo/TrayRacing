3e218ffdbbc5d6e67860861cacf55c687f3cec92
Johan Kwok <johansukwok@gmail.com>
2024-05-29 12:29:54 -0300

    add simple main menu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public void PlayGame () {
+        SceneManager.LoadScene (2);
+    }
+
+    public void PlayOptions () {
+        SceneManager.LoadScene (1);
+    }
+
+    public void MainMenu () {
+        SceneManager.LoadScene (0);
+    }
+
+}
